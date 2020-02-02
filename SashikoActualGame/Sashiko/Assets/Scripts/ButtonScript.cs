b718ed1c085ade7dc2bf0a29a0a589b303928cea
Tabitha Leimonis <leimonistabitha@gmail.com>
2020-02-02 14:08:15 +1100

    Initial commit

---
+++ SashikoActualGame/Sashiko/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Sprite hoverSprite;
+    public Sprite normalSprite;
+    public AudioSource audio;
+
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = this.GetComponent<Button>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        button.onClick.AddListener(delegate () { audio.Play(); });
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.GetComponent<Image>().sprite = hoverSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.GetComponent<Image>().sprite = normalSprite;
+    }
+
+
+}
---
+++ SashikoActualGame/Sashiko/Assets/Scripts/SewPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SewPoint : MonoBehaviour
+{
+    public AudioSource audio;
+    public GameObject controller;
+    public PlayerController controllerScript;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+        controller = GameObject.Find("GameController");
+        controllerScript = controller.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlaySound();
+    }
+
+    public void PlaySound()
+    {
+        if (controllerScript.audioPlay == true)
+        {
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+                controllerScript.audioPlay = false;
+            }
+        }
+    }
+}
