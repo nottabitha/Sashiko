47e583b99a1217ad94a9cbb992df35e442864e64
nottabitha <leimonistabitha@gmail.com>
2024-09-22 20:27:14 +1000

    feat: fixed the button sound from ending early on scene change

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static AudioManager instance;
+    private AudioSource audioSource;
+    private void Awake()
     {
-        
+        if (!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySound(AudioClip clip)
     {
-        
+        audioSource.PlayOneShot(clip);
     }
 }
